d5f2f4946cde5e896673c6ff193746cbb308e9ff
mrVengr <jocoala@mail.ru>
2015-09-27 14:40:27 +0300

    Pattern Strategy (without methods)

---
+++ EveryDayNoteLib/TextNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryDayNoteLib
+{
+    public class TextNote:Notice
+    {
+        public void Create()
+        {
+
+        }
+    }
+}
---
+++ EveryDayNoteLib/VideoNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryDayNoteLib
+{
+    public class VideoNote:Notice
+    {
+        public void Create()
+        {
+
+        }
+    }
+}
